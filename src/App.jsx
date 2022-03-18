50552a9aecb9e10ca7275e31425ac55c9cfce901
Guillermo Farfan <memo56_fb@hotmail.com>
2022-03-18 10:00:12 -0600

    ✅ feat: add responsive header

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,37 @@ function App() {
 	return (
     <>
       <Header />
-      <main className='pt-28 bg-blue-200'>
-        Gumroad Blog
+      <main className='pt-28 xl:pt-0 bg-blue-200'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime assumenda voluptatibus mollitia? Esse, iste, porro perferendis nobis odit ad fugit facere in aperiam sequi aut? Voluptates ipsum reiciendis perferendis possimus?
+        Harum iste rerum explicabo perferendis cumque hic? Aut at ipsum perspiciatis sint eaque sed sequi delectus porro laborum ad deleniti, rerum maxime alias dolores quibusdam omnis! Inventore ducimus nostrum odio!
+        Earum harum, itaque dolor dicta at doloremque officiis consequatur inventore, dolorum qui accusamus! Id enim impedit assumenda eum nobis laudantium. Voluptatibus explicabo facilis, expedita id minus nobis officiis sint. Accusamus.
+        Maxime omnis neque deserunt aliquam nostrum quod id veritatis delectus ab perspiciatis! Sed ipsam maxime minima sequi animi deserunt placeat doloremque veniam ipsa cum, porro ducimus, cupiditate excepturi, enim dolorum?
+        Sit minus assumenda sunt! Debitis rem quo quasi accusantium, voluptatibus et quam sunt esse error ipsam molestiae optio dolor consectetur asperiores perspiciatis voluptas nulla quae. Itaque quod obcaecati quasi quidem!
+        Beatae possimus exercitationem ut praesentium, consequatur deleniti in quisquam, maxime porro inventore quibusdam, reiciendis iure hic magnam eveniet pariatur voluptate similique expedita architecto ipsam deserunt reprehenderit. Facilis neque est reiciendis.
+        Velit fugit officia harum! Doloremque vel voluptates itaque inventore voluptate laborum, cum perspiciatis sapiente magnam qui distinctio corporis tenetur, laudantium a maxime ut iure eligendi quibusdam ipsa dignissimos. Quas, facere.
+        Officia delectus itaque soluta officiis accusamus architecto iure, corporis vitae laborum, a, magnam dolore voluptate blanditiis consectetur nesciunt dicta dolores laboriosam excepturi beatae? Quis alias voluptatibus consequatur. Sequi, itaque iure.
+        Dolor cumque numquam impedit. Facere autem dolor facilis eos voluptate nemo veniam incidunt architecto libero at dicta cum repellat repudiandae quia, dignissimos molestiae consequatur impedit omnis accusantium tempore. Odio, aspernatur!
+        Officia ex impedit culpa esse laborum fugiat totam eius, vero facere corporis atque quis magnam! Rem voluptatem autem quae ad provident facilis incidunt ipsum explicabo iusto, ipsam a ipsa ex!
+        Vero enim, dignissimos sint eaque laborum voluptatum odit totam in praesentium non corporis quasi excepturi magni ex esse, reprehenderit nihil nam veniam repellendus iste maiores modi deleniti similique? Rerum, odit.
+        Quasi, sed excepturi recusandae eos adipisci, eveniet atque quae veritatis quo explicabo in consequatur necessitatibus porro at inventore pariatur! Debitis odit accusamus cumque laudantium quis labore dolores ipsum officiis minus?
+        Odit tenetur excepturi minus veritatis fugit ullam expedita nemo assumenda, cumque at error fugiat animi ex veniam officiis dolorem consequuntur? Veritatis consequuntur vel, reiciendis fugiat quas soluta autem ipsum optio.
+        Magnam doloremque numquam esse quos, dolor, iste labore voluptate ratione aperiam praesentium sint quasi ullam possimus optio inventore nam provident officia! Corrupti eos earum quod accusamus exercitationem labore praesentium eius.
+        Officiis eum est dicta, consectetur magni totam nihil ex odit nesciunt voluptas recusandae incidunt ut animi. Libero quos perferendis, ipsa quasi est quam dignissimos ad, fugiat vitae porro laborum labore?
+        Reprehenderit, nisi autem praesentium deserunt consequatur facere, debitis aliquid alias id unde error voluptate eos? Quis suscipit fugiat a! Quo necessitatibus optio saepe voluptatibus architecto officia odit ullam accusamus nobis.
+        Asperiores culpa possimus, optio accusamus veritatis laborum at? Doloremque, asperiores in et ducimus ab praesentium cumque omnis ex reprehenderit. Voluptas iste doloremque odit ipsa accusantium explicabo culpa magni maxime voluptatibus.
+        Dolore minus doloremque sequi repudiandae cum accusamus libero, facere ex. Tempore non quam ab aperiam blanditiis facere commodi? Nobis repellendus vitae recusandae velit minus officiis necessitatibus porro nulla odit. Architecto!
+        Minus necessitatibus quasi tempore perferendis, blanditiis quia saepe voluptatem! Cum beatae molestiae nihil, excepturi quidem fuga ex maiores temporibus id nam obcaecati! Fugit architecto error asperiores natus doloribus reiciendis harum!
+        Blanditiis sint ratione illo nostrum, veritatis quas aut laboriosam tempora accusamus. Numquam ipsum maiores sed? Minima repellendus praesentium eligendi ipsa quos impedit? Aperiam, provident magni dolore enim velit doloribus expedita?
+        Voluptate sed voluptatibus in illum laboriosam saepe alias repellendus, consectetur magnam fugit sunt, nemo cupiditate atque similique labore dolore laborum fuga est tempore officia corporis. Quis quam praesentium quod optio!
+        Eaque pariatur quae inventore facere et nesciunt nemo eligendi at repellat exercitationem laborum voluptates, eius ex amet tempora non, culpa molestiae beatae ipsum placeat ab quidem, quibusdam atque. Ducimus, voluptatibus.
+        Nostrum perspiciatis minus sunt possimus. Fugit, inventore laborum. Fuga modi assumenda quo pariatur earum atque ducimus voluptatum ratione nobis dignissimos odit, explicabo beatae distinctio autem quidem sapiente architecto accusamus amet?
+        Laudantium deleniti pariatur ratione nobis. Rerum fuga est asperiores incidunt! Quas dicta, non animi ipsam alias eius? Suscipit est vero explicabo veritatis quod, nihil sequi quidem facere voluptas excepturi dicta!
+        Expedita, tempore doloribus, rerum laboriosam ducimus officiis omnis fugiat consectetur nam amet qui. Officia modi libero dolorem cum, aliquam illum omnis accusantium enim blanditiis? Quasi alias nihil quis dolore voluptatem!
+        Officia corrupti voluptatum tenetur autem neque! Temporibus commodi incidunt amet veniam error beatae consectetur, ullam nesciunt voluptates nobis magni maxime, a, facilis autem omnis minima odit adipisci ratione officia iure!
+        Aut maiores facere dicta dolor? Doloribus iusto magni inventore est, ea optio nesciunt aperiam sed praesentium quod repudiandae architecto necessitatibus cupiditate tempora ad perferendis commodi voluptate quidem. Accusamus, exercitationem ipsam.
+        Sed ullam eaque ratione officiis magnam aspernatur assumenda exercitationem libero quidem! Perferendis laudantium, fuga ex velit nostrum ut eaque exercitationem voluptates dolore, porro quaerat, autem aut? Quam odit libero ad?
+        Totam cupiditate quis maxime officia nobis ea odit in quasi repellat corrupti animi tempora molestias dolores cum, veritatis architecto reprehenderit sed id, minus officiis eius voluptatibus iure dignissimos! Vitae, odio?
+        Dolores, minus. Ipsa vel veritatis minus repudiandae, tempore deleniti, fugit, tenetur dolore sint eius eum blanditiis facilis esse qui? Dolorum enim consectetur aperiam eveniet, alias possimus omnis itaque eos fuga.
       </main>
       <Footer />
     </>
